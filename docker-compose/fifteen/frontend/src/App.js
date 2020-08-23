b149361e6addbe2ad69119af18bd6b79f615c624
LightOfMooN <vlad@salehov.ru>
2020-08-24 00:56:23 +0500

    added App

--- docker-compose/fifteen/frontend/src/App.js
+++ docker-compose/fifteen/frontend/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [gameState, setGameState] = useState();
+
+  const callApi = (url) => {
+    fetch(url)
+      .then(response => response.json())
+      .then(result => {
+          setGameState(result);
+        }
+      );
+  };
+
+  useEffect(
+    () => callApi('/api/game_info'),
+    []
   );
+
+  return <>
+    {
+      gameState && gameState['values']
+    }
+  </>;
 }
 
 export default App;
