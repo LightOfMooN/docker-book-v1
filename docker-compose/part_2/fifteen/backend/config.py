580d45e665032b795031c7d3ff8c9a0f7744ea92
LightOfMooN <vlad@salehov.ru>
2020-09-08 02:39:22 +0500

    port updated

--- docker-compose/part_2/fifteen/backend/config.py
+++ docker-compose/part_2/fifteen/backend/config.py
@@ -8,7 +8,7 @@ REDIS_HOST = os.environ.get('REDIS_HOST', 'cache')
 REDIS_PORT = os.environ.get('REDIS_PORT', '6379')
 
 DB_HOST = os.environ.get('DB_HOST')
-DB_PORT = os.environ.get('DB_PORT')
+DB_PORT = os.environ.get('DB_PORT', '5432')
 DB_NAME = os.environ.get('DB_NAME', 'fifteen')
 DB_USER = os.environ.get('DB_USER', 'fifteen')
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
