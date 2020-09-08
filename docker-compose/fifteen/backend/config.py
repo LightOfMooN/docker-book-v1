825dac901772715f059c2727019733913d079cdd
LightOfMooN <vlad@salehov.ru>
2020-09-09 03:00:35 +0500

    fifteen updated

--- docker-compose/fifteen/backend/config.py
+++ docker-compose/fifteen/backend/config.py
@@ -1,12 +1,26 @@
 import os
 import redis
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 DEBUG_MODE = str(os.environ.get('DEBUG_MODE')).lower() in {'true', '1', 'yes'}
 REDIS_HOST = os.environ.get('REDIS_HOST', 'cache')
 REDIS_PORT = os.environ.get('REDIS_PORT', '6379')
 
+DB_HOST = os.environ.get('DB_HOST')
+DB_PORT = os.environ.get('DB_PORT', '5432')
+DB_NAME = os.environ.get('DB_NAME', 'fifteen')
+DB_USER = os.environ.get('DB_USER', 'fifteen')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
+DB_ACTIVE = bool(DB_HOST)
+
 app = Flask(__name__)
+
+if DB_ACTIVE:
+    app.config['SQLALCHEMY_DATABASE_URI'] = (
+        f'postgres://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+    )
+db = SQLAlchemy(app)
 
 redis_client = redis.Redis(
     host=REDIS_HOST,
